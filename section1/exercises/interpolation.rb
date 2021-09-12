f3fc3703bfadd142ff9e5fc5017d43730f8c4192
Russell Rockwood <russell.rockwood@gmail.com>
2021-09-12 18:10:30 -0500

    Complete section1

--- section1/exercises/interpolation.rb
+++ section1/exercises/interpolation.rb
@@ -14,17 +14,23 @@ p "The #{team} are #{name}'s favorite Quidditch team"
 # "The quick red fox jumped over the lazy brown dog":
 speedy = "quick red fox"
 slow_poke = "lazy brown dog"
-
-p # YOUR CODE HERE
+p "The #{speedy} jumped over the #{slow_poke}"
 
 # Write code that uses the variables below to form a string that reads
 # "In a predictable result, the tortoise beat the hare!":
 slow_poke = "tortoise"
 speedy = "hare"
 
-# YOUR CODE HERE
+puts "In a predictable result, the #{speedy} beat the #{slow_poke}!"
 
 
 # YOU DO:
-# Declare three variables, name/content/data type of your choice. Think carefully about what you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!) Then, log out ONE sentence that incorporates all THREE variables.
+# Declare three variables, name/content/data type of your choice. Think carefully about what you name the variables.
+# Remember, the goal is to be concise but descriptive (it's a hard balance!)
+# Then, log out ONE sentence that incorporates all THREE variables.
 
+name = "Billy Bob"
+action = "download malicious software from the darkweb"
+number = 3
+
+puts "#{name} decided to #{action} #{number} times. What a weirdo."
--- section1/exercises/variables.rb
+++ section1/exercises/variables.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section1/exercises/variables.rb`
 
 # Example: Write code that saves your name to a variable and
@@ -11,49 +11,64 @@ p name
 # Write code that saves the string 'Dobby' to a variable and
 # prints what that variable holds to the terminal:
 house_elf = "Dobby"
-# YOUR CODE HERE
+p house_elf
 
 # Write code that saves the string 'Harry Potter must not return to Hogwarts!'
 # and prints what that variable holds to the terminal:
-# YOUR CODE HERE
+myString = 'Harry Potter must not return to Hogwarts!'
+p myString
 
 # Write code that adds 2 to the `students` variable and
 # prints the result:
 students = 22
-# YOUR CODE HERE
+students += 1
 p students
 
 # Write code that subracts 2 from the `students` variable and
 # prints the result:
-# YOUR CODE HERE
+students -= 1
 p students
 
 
 # YOU DO:
-# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`. 
+# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`.
+first_name = "Russell"
+is_hungry = true
+number_of_pets = 1
 # Store the appropriate data types in each.
 # print all three variables to the terminal.
-
+p first_name
+p is_hungry
+p number_of_pets
 # IN WORDS:
-# How did you decide to use the data type you did for each of the three variables above? 
+# How did you decide to use the data type you did for each of the three variables above?
 
-# Explain.
+# Aside from Elon Musks son, names are usually a collection of letters so used a string for 'first_name'
+# The variable name is_hungry would likely be answered with a yes or no/true or false. Therefore I chose to use the boolean data type.
+# number_of_pets has the word 'number' in it so an integer made sense.
 
 
 # YOU DO:
 # Re-assign the values to the three variables from the previous challenge to different values (but same data type).
 # print all three variables to the terminal.
+first_name = "Freddy"
+is_hungry = false
+number_of_pets = 321
 
+p first_name
+p is_hungry
+p number_of_pets
 
 # YOU DO:
 # Using the variables below, print the total number of snacks to the terminal:
 healthy_snacks = 6;
 junk_food_snacks = 8;
 
+puts "We have a total of #{healthy_snacks + junk_food_snacks} snacks available."
 
 #-------------------
 # FINAL CHECK
 #-------------------
 
-# Did you run this file in your terminal to make sure everything printed out to the terminal 
-  # as you would expect? 
+# Did you run this file in your terminal to make sure everything printed out to the terminal
+  # as you would expect?
